61f556f11e824ae2cc0f34c39bc77bca466a66bc
Federicaye <gallofederica58@gmail.com>
2024-03-18 16:40:10 +0100

    random number

--- resource/script.js
+++ resource/script.js
@@ -1,8 +1,11 @@
 function createSquares(number, className) { /* funzione per creare un numero di quadrati */
+    let list = [];
+    generateRandomNumber(0, number, list);
     let squares = []; /* array che contine i div */
     for (let i = 0; i < number; i++) { /* ciclo che si ripete un number (argomento) di volte */
         let square = document.createElement("div");  /* variabili che contiene i div */
-        let randomContent = Math.random() < 0.8 ? "fiore" : "bomba";
+        let randomContent = list.includes(i) ? "bomba" : "fiore";
+        /* Math.random() < 0.8 ? "fiore" : "bomba"; */
         /* square.innerHTML = `<span> ${randomContent}</span>`; */
         let content = document.createElement("span"); /* variabile che contiene gli span */
         content.textContent = randomContent;
@@ -13,7 +16,7 @@ function createSquares(number, className) { /* funzione per creare un numero di 
         content.classList.add("d-none");
         square.addEventListener("click", function () {
             content.classList.remove("d-none")
-            console.log(square.id);
+            /*  console.log(square.id); */
         });
         squares.push(square);
     }
@@ -32,14 +35,14 @@ play.addEventListener("click", function () {
     let squares = [];
     if (difficulty.value === "difficoltà1") {
         squares = createSquares(100, "facile");
-        console.log(squares);
+        /*  console.log(squares); */
 
     } else if (difficulty.value === "difficoltà2") {
         squares = createSquares(81, "medio");
-        console.log(squares);
+        /* console.log(squares); */
     } else if (difficulty.value === "difficoltà3") {
         squares = createSquares(49, "difficile");
-        console.log(squares);
+        /*  console.log(squares); */
     }
     campoMinato.innerHTML = "";
     squares.forEach(element => {
@@ -79,12 +82,12 @@ function generateRandomNumber(min, max, lista) {
     return number;
 }
 
-let list = [];
+/* let list = [];
 generateRandomNumber (3,9, list);
 console.log(list);
 
-for (let i=0; i<5; i++){
+for (let i=0; i<16; i++){
     generateRandomNumber(1,100,list)
 }
 
-console.log(list);
+console.log("lista " + list); */
